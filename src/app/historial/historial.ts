ba3b10d98f93d345bc8ec0d4cf72909706c925b1
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-06-09 11:54:16 -0500

    Commit foto

--- src/app/historial/historial.ts
+++ src/app/historial/historial.ts
@@ -1,28 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-historial',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './historial.html',
-  styleUrls: ['./historial.css']
+  templateUrl: './historial.component.html'
 })
 export class HistorialComponent implements OnInit {
-  fotos: string[] = [];
+  fotos: any[] = [];
+  error: string = '';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get<any[]>(`${environment.apiUrl}/gallery`).subscribe({
-      next: (res) => {
-        this.fotos = res.map(foto => foto.url);
-      },
-      error: () => {
-        alert('Error al cargar la galería');
-      }
+    this.http.get<any[]>('http://localhost:3000/gallery').subscribe({
+      next: (data) => this.fotos = data,
+      error: (err) => this.error = 'Error al cargar imágenes'
     });
   }
 }
